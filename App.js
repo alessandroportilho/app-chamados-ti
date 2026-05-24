e7eb7b77869b3c515cd07a14c136b58731ea050c
Caio689 <caio.clm89@gmail.com>
2026-05-24 16:49:49 -0300

    retirar alerta durantes teste

--- App.js
+++ App.js
@@ -6,13 +6,14 @@ import {
   FlatList,
   TouchableOpacity,
   StyleSheet,
-  SafeAreaView,
   Dimensions,
   Alert,
   ActivityIndicator,
   Image,
   ScrollView,
 } from "react-native";
+
+import { SafeAreaView } from "react-native-safe-area-context";
 
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
